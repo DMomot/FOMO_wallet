ef71bee0e979ede0d8fe74922c4564a4020c8414
lainiwa <kirrik96@gmail.com>
2023-11-18 22:24:04 +0400

    Add commented-out openapi_url

--- backend/src/backend/main.py
+++ backend/src/backend/main.py
@@ -3,7 +3,9 @@ from fastapi import FastAPI
 from backend.external_services.one_inch_devportal.full_info import get_address_info
 from backend.models import AddressType
 
-app = FastAPI()
+app = FastAPI(
+    # openapi_url="/api/openapi.json",
+)
 
 
 @app.get("/{address}")
